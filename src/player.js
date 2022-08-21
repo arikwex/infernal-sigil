268ab185ae6b64b6b067448fa1ca8a7bb7385dc9
Ariel Wexler <arielwex@gmail.com>
2022-08-21 01:20:44 -0400

    Critter has some life

--- src/player.js
+++ src/player.js
@@ -2,8 +2,11 @@ import { color, renderMesh } from './canvas';
 
 function Player(x, y) {
     const pos = { x, y };
+    const thickness = 9;
+    let anim = 0;
+
     const headMesh = [
-        ['#e22', 10, 0],
+        ['#e22', thickness, 0],
         [14, -40, 18, -26, 1, 0, -1, 0, -18, -26, -14, -40],
         [-7, -11, 7, -11],
         ['#fff', 4, 5],
@@ -11,19 +14,42 @@ function Player(x, y) {
         [9, -11, 3, -9],
     ];
     const bodyMesh = [
-        ['#e22', 10, 0],
+        ['#e22', thickness, 0],
         [0, 0, 0, 20],
         [0, 20, 7, 37],
         [0, 20, -7, 37],
     ];
+    const handMesh = [
+        ['#e22', thickness, 0],
+        [4, 0, 14, 6],
+        ['#ee2', 3, 0],
+        [18, 5, 22, 8],
+        [15, 9, 19, 13],
+    ];
+    const tailMesh = [
+        ['#e22', thickness, 0],
+        [0, 20, -16, 31, -26, 31, -32, 22, -34, 15]
+    ];
 
     function update(dT) {
+        anim += dT;
     }
 
     function render(ctx) {
-        const t = -0.6 + Math.cos(Date.now() * 0.001);
-        renderMesh(bodyMesh, { x: pos.x, y: pos.y - 4, z: 0 }, { t: t, p: 0.1 });
-        renderMesh(headMesh, { x: pos.x, y: pos.y, z: 9 }, { t: t, p: -0.1 });
+        const t = Math.cos(anim) * 0.7;
+        const a = anim * 6;
+
+        renderMesh(tailMesh, { x: pos.x, y: pos.y - 8, z: 0 }, { t: t + 1.57 + t * 0.3, p: 0 });
+        renderMesh(handMesh, { x: pos.x, y: pos.y - Math.cos(a + 3) * 1.5 + 1, z: 0 }, { t: t, p: 0 });
+        renderMesh(bodyMesh, { x: pos.x, y: pos.y - 8, z: 0 }, { t: t, p: 0 });
+        renderMesh(handMesh, { x: pos.x, y: pos.y - Math.cos(a + 3) * 1.5 + 1, z: 0 }, { t: t+3.14, p: 0 });
+        renderMesh(headMesh, { x: pos.x, y: pos.y + Math.cos(a + 1) * 1.5 + 1, z: 10 }, { t: t, p: t*0.3 });
+
+        //[0, 20, -16, 31, -26, 31, -32, 22, -34, 15]
+        tailMesh[1][3] = Math.cos(a) * 1 + 31;
+        tailMesh[1][5] = Math.cos(a + 1) * 1 + 31;
+        tailMesh[1][7] = Math.cos(a + 2) * 2 + 22;
+        tailMesh[1][9] = Math.cos(a + 3) * 1 + 15;
     }
 
     return {
