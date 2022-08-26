105ea1c35162998cbc178243e55a4740514224fa
Ariel Wexler <arielwex@gmail.com>
2022-08-26 12:04:57 -0400

    Vertical map decimator

--- src/map.js
+++ src/map.js
@@ -6,6 +6,7 @@ import { BoundingBox } from './bbox';
 function Map() {
     let img = new Image();
     let data = null;
+    let W;
 
     async function generate() {
         await new Promise((r) => {
@@ -17,26 +18,52 @@ function Map() {
         let context = canvas.getContext('2d');
         context.drawImage(img, 0, 0);
 
-        const W = img.width;
+        W = img.width;
         const H = img.height;
         data = context.getImageData(0, 0, W, H).data;
         console.log(data);
         context.clearRect(0, 0, W, H);
         
         const BLOCK_SIZE = 100;
+
+        // Character placements
         for (let x = 0; x < W; x++) {
             for (let y = 0; y < H; y++) {
                 const baseIdx = (x + y * W) << 2;
-                const V = (data[baseIdx] << 16) | (data[baseIdx + 1] << 8) | data[baseIdx + 2];
-                if (V == 0xffffff) {
-                    add({ tags: ['physics'], physics: new BoundingBox((x - 0.5) * BLOCK_SIZE, (y - 0.5) * BLOCK_SIZE, 0, 0, BLOCK_SIZE, BLOCK_SIZE) });
-                }
+                const V = get(x, y);
+                // if (V == 0xffffff) {
+                //     //add({ tags: ['physics'], physics: new BoundingBox((x - 0.5) * BLOCK_SIZE, (y - 0.5) * BLOCK_SIZE, 0, 0, BLOCK_SIZE, BLOCK_SIZE) });
+                // }
                 if (V == 0x00ff00) {
                     getObjectsByTag('player')[0].move(x * BLOCK_SIZE, y * BLOCK_SIZE);
                 }
             }
         }
-        // add({ tags: ['physics'], physics: new BoundingBox(0, 440, 0, 0, 800, 100) });
+
+        // Merge walls vertically
+        const usedMap = {};
+        for (let x = 0; x < W; x++) {
+            for (let y = 0; y < H; y++) {
+                const V = get(x, y);
+                if (V == 0xffffff && !usedMap[x+','+y]) {
+                    let q = y;
+                    while (q < 128) {
+                        const V2 = get(x, q);
+                        if (V2 != 0xffffff) {
+                            break;
+                        }
+                        usedMap[x+','+q] = true;
+                        q++;
+                    }
+                    add({ tags: ['physics'], physics: new BoundingBox((x - 0.5) * BLOCK_SIZE, (y - 0.5) * BLOCK_SIZE, 0, 0, BLOCK_SIZE, (q - y) * BLOCK_SIZE) });
+                }
+            }
+        }
+    }
+
+    function get(x, y) {
+        const baseIdx = (x + y * W) << 2;
+        return (data[baseIdx] << 16) | (data[baseIdx + 1] << 8) | data[baseIdx + 2]
     }
 
     return {
