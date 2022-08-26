35a67392c7badc1a7df68dfa0b8f9166058b9a64
Ariel Wexler <arielwex@gmail.com>
2022-08-26 16:45:18 -0400

    Cute decoration ftw

--- src/main.js
+++ src/main.js
@@ -8,35 +8,9 @@ import Map from './map';
 async function initialize() {
     const m = new Map();
     
-    // const player = new Player(150, 400);
-    // add(player);
-    
     await m.generate();
     add(m);
     add(new HUD());
-    // add(new Camera(player.playerHitbox.x, player.playerHitbox.y));
-
-    // add(new Skeleton(500, 439));
-    // // add(new Skeleton(300, 339));
-    // // add(new Skeleton(400, 239));
-    // add(new Bone(300, 300, 0, 0));
-    // add(new HUD());
-
-    // // L-SHAPE LAYOUT
-    // add({ tags: ['physics'], physics: new BoundingBox(0, 440, 0, 0, 800, 100) });
-    // add({ tags: ['physics'], physics: new BoundingBox(650, 100, 0, 0, 100, 340) });
-    // add({ tags: ['physics'], physics: new BoundingBox(250, 340, 0, 0, 100, 100) });
-
-    // // ISLANDS LAYOUT
-    // add({ tags: ['physics'], physics: new BoundingBox(0+1000, 440, 0, 0, 300, 100) });
-    // add({ tags: ['physics'], physics: new BoundingBox(400+1000, 440, 0, 0, 300, 100) });
-    // add({ tags: ['physics'], physics: new BoundingBox(100+1000, 140, 0, 0, 100, 200) });
-    // add({ tags: ['physics'], physics: new BoundingBox(500+1000, 140, 0, 0, 100, 300) });
-
-    // // NOOK LAYOUT
-    // add({ tags: ['physics'], physics: new BoundingBox(0+1000*2, 440, 0, 0, 800, 100) });
-    // add({ tags: ['physics'], physics: new BoundingBox(350+1000*2, 100, 0, 0, 100, 340) });
-    // add({ tags: ['physics'], physics: new BoundingBox(0+1000*2, 100, 0, 0, 350, 100) });
 
     // Game events
     bus.on('bone:spawn', ([x,y,N]) => {
--- src/wall.js
+++ src/wall.js
@@ -4,9 +4,18 @@ import { renderMesh } from "./canvas";
 function Wall(x, y, w, h) {
     const physics = new BoundingBox(x, y, 0, 0, w, h);
     const wallMesh = [
-        ['#fff', 5, 0]
+        ['#a99', 5, 0]
     ];
-    wallMesh.push([0, 0, w, 0, w, h, 0, h, 0, 0]);
+    //wallMesh.push([0, 0, w, 0, w, h, 0, h, 0, 0]);
+    let outline = [];
+    let dx = Math.random() * 6 + 6;
+    for (let i = 0; i < w; i += dx) {
+        outline.push(i, Math.random() * 6);
+        outline.push(Math.min(w, i + dx), -Math.random() * 2);
+        dx = Math.random() * 6 + 6;
+    }
+    outline.push(w, 0, w, h, 0, h, 0, 0);
+    wallMesh.push(outline);
 
     function render(ctx) {
         renderMesh(wallMesh,x,y,0,0,0);
