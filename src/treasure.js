3391001f4c2a9179327d4bf731819f8e9b8145bd
Ariel Wexler <arielwex@gmail.com>
2022-09-08 15:51:08 -0400

    Minor treasure optimization

--- src/treasure.js
+++ src/treasure.js
@@ -6,10 +6,10 @@ import { inView } from './utils';
 
 // Values -> 20, 50, 100
 // Hp -> 3, 6, 9 (1 bone each)
-const boneMap = {1: 7, 2: 14, 3: 11};
-const skullMap = {1: 1, 2: 3, 3: 7 };
-const colorMap = {1: '#a63', 2: '#889', 3: '#db1'};
-const bgColorMap = {1: '#742', 2: '#667', 3: '#b90'};
+const boneMap = [7, 14, 11];
+const skullMap = [1, 3, 7];
+const colorMap = ['#a63', '#889', '#db1'];
+const bgColorMap = ['#742', '#667', '#b90'];
 
 function Treasure(x, y, t) {
     y += 50;
@@ -18,8 +18,8 @@ function Treasure(x, y, t) {
     let hp = 3 * t;
     const myHitbox = new BoundingBox(x-25,y-55,0,0,50,55);
 
-    const baseColor = colorMap[t];
-    const bgColor = bgColorMap[t];
+    const baseColor = colorMap[t-1];
+    const bgColor = bgColorMap[t-1];
     const treasureMesh = [
         [baseColor, 8, 0],
         [20, 0, -20, 0, -28, -23, 28, -23, 20, 0],
@@ -31,8 +31,8 @@ function Treasure(x, y, t) {
 
     function update(dT) {
         if (hp <= 0) {
-            bus.emit('bone:spawn', [x,y-20, boneMap[t], 1]);
-            bus.emit('bone:spawn', [x,y-20, skullMap[t], 2]);
+            bus.emit('bone:spawn', [x,y-20, boneMap[t-1], 1]);
+            bus.emit('bone:spawn', [x,y-20, skullMap[t-1], 2]);
             return true;
         }
         hitTimer = Math.max(hitTimer - dT, 0);
