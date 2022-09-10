60439ede12fd8dae5b710790e5b35b4eaa7d435e
Ariel Wexler <arielwex@gmail.com>
2022-09-10 13:03:26 -0400

    Cleanup fireball to leverage flameSFX

--- src/fireball.js
+++ src/fireball.js
@@ -1,29 +1,26 @@
 import * as bus from './bus';
 import { BoundingBox } from "./bbox";
 import { color, renderMesh } from "./canvas";
-import { getObjectsByTag } from "./engine";
 import { clamp, physicsCheck } from "./utils";
 import { EVENT_ATTACK, EVENT_ATTACK_HIT, EVENT_SFX_FLAME } from './events';
-import { TAG_PHYSICS } from './tags';
+import FlameSFX from './flame-sfx';
+import { add, remove } from './engine';
 
 function Fireball(x, y, dir) {
     let vx = 300 * dir;
     x += dir * 40;
     let self = this;
     let lifetime = 0;
+    let flame = FlameSFX(x, y, 2, 10, dir * 1.57);
+    add(flame);
 
-    const ballMesh = [
-        ['#fb1', 10, 0],
-        [10, 0, 0, -10, -10, 0, -15, -5, -20, 0, -28, 0, -20, 0, -15, 5, -10, 0, 0, 10, 10, 0],
-        ['#f82', 10, 0],
-        [2, 0, -4, 0],
-    ];
     const myHitbox = BoundingBox(x, y, -20, -15, 30, 30);
 
     function update(dT) {
         lifetime += dT;
         if (!self || lifetime > 9) {
             bus.off(EVENT_ATTACK_HIT, onAttackHit);
+            flame.end();
             return true;
         }
         // TBD if this can be less aggressive
@@ -31,24 +28,14 @@ function Fireball(x, y, dir) {
         let _, onGround, onRight, onLeft, onRoof;
         [_, _, onGround, onRight, onLeft, onRoof] = physicsCheck(myHitbox);
         if (onGround || onRight || onLeft || onRoof) {
-            bus.emit(EVENT_SFX_FLAME, [x, y, 2, 0.5]);
             bus.off(EVENT_ATTACK_HIT, onAttackHit);
+            flame.end();
             return true;
         }
         vx = clamp(vx + 1000 * dT * dir, -750, 750);
         x += vx * dT;
         myHitbox.x = x;
-    }
-
-    function render(ctx) {
-        color('#fb1');
-        ctx.beginPath();
-        const t = Date.now();
-        const p = (t % 100) / 100;
-        let s = (p - p * p) * 40;
-        ctx.arc(x - (5 + p * 42) * dir, y + Math.cos(t/20)*4, s, 0, 6.28);
-        ctx.fill();
-        renderMesh(ballMesh, x, y, 0, 0, dir * 1.57 - 1.57);
+        flame.set(x);
     }
 
     function onAttackHit([owner]) {
@@ -61,7 +48,6 @@ function Fireball(x, y, dir) {
 
     return {
         update,
-        render,
     }
 }
 
--- src/flame-sfx.js
+++ src/flame-sfx.js
@@ -1,6 +1,6 @@
 import { clamp, inView } from "./utils";
 
-function FlameSFX(x, y, scale, lifetime) {
+function FlameSFX(x, y, scale, lifetime, rot = 0) {
     let anim = 0;
     let baseScale = scale;
 
@@ -18,23 +18,29 @@ function FlameSFX(x, y, scale, lifetime) {
     }
 
     function render(ctx) {
+        const xfm = ctx.getTransform();
+        ctx.translate(x, y);
+        ctx.rotate(-rot);
         ctx.fillStyle = '#f93';
         for (let i = 0; i < 6; i++) {
             let p = ((i + anim) % 6) / 6;
             let s = scale * clamp(10 * p / (1 + p * p * 15) * (1 - p), 0, 1);
             ctx.beginPath();
-            ctx.arc(x + Math.cos(i * 4) * scale, y - p * 40 * scale, 10 * s, 0, 6.28);
+            ctx.arc(Math.cos(i * 4) * scale, - p * 40 * scale, 10 * s, 0, 6.28);
             ctx.fill();
         }
         ctx.fillStyle = '#f53';
         ctx.beginPath();
-        ctx.arc(x + Math.cos(anim * 13) * scale / 2, y - scale * 4, 5 * scale * (1 + Math.cos(anim * 11) * 0.1), 0, 6.28);
+        ctx.arc(Math.cos(anim * 13) * scale / 2, - scale * 4, 5 * scale * (1 + Math.cos(anim * 11) * 0.1), 0, 6.28);
         ctx.fill();
+        ctx.setTransform(xfm);
     }
 
     return {
         update,
         render,
+        set: (a) => {x=a},
+        end: () => {lifetime=0.25},
         inView: (cx, cy) => inView(x, y, cx, cy),
         order: -4500
     }
